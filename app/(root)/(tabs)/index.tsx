1855d65f405f9690157920e4827b7007ac6931d1
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2025-01-31 15:34:37 +0545

    initial setup with nativewind

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/sign-in" className="bg-red-500">
+        Sign In
+      </Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Properties</Link>
+    </View>
+  );
+}
